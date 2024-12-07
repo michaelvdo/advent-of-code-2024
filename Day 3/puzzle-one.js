ef4e93c9a5d2759ecd113c86339085424097a3bb
Michael van den Oudenalder <michaelvdo@gmail.com>
2024-12-07 19:45:22 +0100

    Day 3-1 rewrite.

--- Day 3/puzzle-one.js
+++ Day 3/puzzle-one.js
@@ -3,13 +3,8 @@ import { data } from "./data.js";
 const mul = (a, b) => a * b;
 
 const getMul = (testString) => {
-  const muls = testString.match(/mul[(][0-9]{1,3},[0-9]{1,3}\)/g);
-
-  const sum = muls.reduce((acc, curr) => {
-    return acc + eval(curr);
-  }, 0);
-
-  return sum;
+  const muls = testString.match(/mul\([0-9]{1,3},[0-9]{1,3}\)/g);
+  return muls.reduce((acc, curr) => acc + eval(curr), 0);
 };
 
 console.log(getMul(data));
